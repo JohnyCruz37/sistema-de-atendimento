651c475492ad0f3bcd3a4afccea4f228c3d18cc3
Johny Cruz <johnygoncalvescruz@gmail.com>
2022-06-29 15:49:41 -0300

    gerando senha nas arrays filaN e filaP e mostrando senha no ticket

--- js/script.js
+++ js/script.js
@@ -8,13 +8,17 @@ const btnGerenciador = document.querySelector(".gerenciador");
 
 // ELEMENTOS AREA DO TICKET
 const tipoTicket = document.querySelector('.gera-senha-ticket-tipo');
-const dataTicket = document.querySelector('.gera-senha-ticket-data')
+const dataTicket = document.querySelector('.gera-senha-ticket-data');
+const numeroTicket = document.querySelector('.gera-senha-ticket-numero')
 
 //ELEMENTOS DO GERENCIADOR DE SENHA
 const gerenciador = document.querySelector(".area-gerenciador");
 const xGerenciador = document.querySelector(".fecha-gerenciador");
 const btnGNormal = document.querySelector(".gerenciador-normal");
 const btnGPreferencial = document.querySelector(".gerenciador-preferencial");
+const filaTodos = document.querySelector('.div-fila');
+const filaPreferencial = document.querySelector('.div-preferencial');
+const filaNormal = document.querySelector('.div-normal');
 
 
 
@@ -26,21 +30,88 @@ const tipoMonitor = document.querySelector('.tipo-monitor');
 
 var data = new Date();
 
-// FUNÇÕES
+//ARRAY
+const fila = [];
+const filaN = [];
+const filaP = [];
 
-// GERADOR DE SENHA
-const gerarSenha = (botao) => {
-    alert("VOCÊ GEROU UMA SENHA DO TIPO: " + botao.textContent);
-    tipoTicket.innerText = botao.textContent;
-    dataTicket.innerText = data.getDay(); 
+//METODO
+//Mostrar dia da semana em texto
+const diaSemanaTexto = (semana, elemento) =>{
+    
+  if(semana === 0) {
+    elemento.innerText='Domingo';
+  }
+  if(semana === 1) {
+    elemento.innerText='Segunda-feira';
+  }
+  if(semana === 2) {
+    elemento.innerText='Terça-feira';
+  }
+  if(semana === 3) {
+    elemento.innerText='Quarta-feira';
+  }
+  if(semana === 4) {
+    elemento.innerText='Quinta-feira';
+  }
+  if(semana === 5) {
+    elemento.innerText='Sexta-feira';
+  }
+  if(semana === 6) {
+    elemento.innerText='sábado';
+  }
 
+}
+
+//CRIANDO O TEXTO DA SENHA
+const senhaTexto = (p, array, texto) => {
+  if(array.length < 10){
+    texto.innerText = p + '00' + array.length;
+  }else if(array.length < 100){
+    texto.innerText = p + '0'+array.length;
+  } else{
+    texto.innerText = p + array.length;
+  }
+
+
+}
+
+//ADICIONANDO PREFIXO A SENHA
+const tipoSenha = (botao, texto) => {
     let prefixo;
-
     if(botao.textContent === 'Preferencial'){
       prefixo = 'P -';
+
+      filaP.push(filaP.length + 1)
+      senhaTexto(prefixo, filaP, texto);
+
+
     }else{
       prefixo = 'N -';
+
+      filaN.push(filaN.length + 1);
+      senhaTexto(prefixo, filaN, texto);
+
     }
+}
+
+// FUNÇÕES
+// GERADOR DE SENHA
+const gerarSenha = (botao) => {
+    //FILA
+    console.log(fila)
+    //TIPO DE ATENDIMENTO
+    tipoTicket.innerText = botao.textContent;
+
+    //NÚMERO SENHA
+    tipoSenha(botao, numeroTicket);
+    
+    //DATA SENHA
+    diaSemanaTexto(data.getDay(), dataTicket);
+
+    //PREVISÃO DE ESPERA SENHA
+  
+    
 
 }
 
@@ -58,49 +129,31 @@ const fechaGerenciador = () => {
 
 
 
-//Mostrar dia da semana
-const diaSemanaTexto = () =>{
-    const semana = data.getDay();
-    
-    if(semana === 0) {
-      diaSemana.innerText='Domingo';
-    }
-    if(semana === 1) {
-      diaSemana.innerText='Segunda-feira';
-    }
-    if(semana === 2) {
-      diaSemana.innerText='Terça-feira';
-    }
-    if(semana === 3) {
-      diaSemana.innerText='Quarta-feira';
-    }
-    if(semana === 4) {
-      diaSemana.innerText='Quinta-feira';
-    }
-    if(semana === 5) {
-      diaSemana.innerText='Sexta-feira';
-    }
-    if(semana === 6) {
-      diaSemana.innerText='sábado';
-    }
 
-}
 
 
 
 
 //BOTÕES
+
+//botões do gerador de senha
 btnNormal.addEventListener('click', () =>{
   gerarSenha(btnNormal);
-});
-btnGNormal.addEventListener('click', () =>{
-  gerarSenha(btnGNormal);
 });
 btnPreferencial.addEventListener('click', () =>{
   gerarSenha(btnPreferencial);
 });
+
+//botões do gerenciador de senha
+btnGerenciador.addEventListener('click', abreGerenciador);
+xGerenciador.addEventListener('click', fechaGerenciador);
+
+btnGNormal.addEventListener('click', () =>{
+  gerarSenha(btnNormal);
+});
+
 btnGPreferencial.addEventListener('click', () =>{
-  gerarSenha(btnGPreferencial);
+  gerarSenha(btnPreferencial);
 });
 
 
@@ -109,7 +162,5 @@ btnGPreferencial.addEventListener('click', () =>{
 hora.innerText = data.getHours() + " : " + data.getMinutes(); 
 
 //MOSTRAR DIA DA SEMANA
-diaSemanaTexto();
-btnGerenciador.addEventListener('click', abreGerenciador);
-xGerenciador.addEventListener('click', fechaGerenciador);
+diaSemanaTexto(data.getDay(), diaSemana);
 
